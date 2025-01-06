59b40f2e43ba8483ad6997e348255e654cff8e34
Fajar Alif <fajaralif@icloud.com>
2025-01-07 02:43:26 +0700

    Use MappedByteBuffer

--- src/main/java/dev/morling/onebrc/CalculateAverage_maarlf.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_maarlf.java
@@ -15,74 +15,126 @@
  */
 package dev.morling.onebrc;
 
-import static java.util.stream.Collectors.*;
-
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Map;
+import java.nio.file.StandardOpenOption;
 import java.util.TreeMap;
-import java.util.stream.Collector;
 
 public class CalculateAverage_maarlf {
 
     private static final String FILE = "./measurements.txt";
 
-    private static record Measurement(String station, double value) {
-        private Measurement(String[] parts) {
-            this(parts[0], Double.parseDouble(parts[1]));
+    private static record ResultRow(
+        double min,
+        double max,
+        double sum,
+        int count
+    ) {
+        public ResultRow(double measurement) {
+            this(measurement, measurement, measurement, 1);
         }
-    }
 
-    private static record ResultRow(double min, double mean, double max) {
-        public String toString() {
-            return round(min) + "/" + round(mean) + "/" + round(max);
+        public ResultRow merge(ResultRow other) {
+            double newMin = Math.min(this.min, other.min);
+            double newMax = Math.max(this.max, other.max);
+            double newSum = this.sum + other.sum;
+            int newCount = this.count + other.count;
+            return new ResultRow(newMin, newMax, newSum, newCount);
+        }
+
+        public double mean() {
+            return round(sum) / count;
         }
 
         private double round(double value) {
             return Math.round(value * 10.0) / 10.0;
         }
-    }
 
-    private static class MeasurementAggregator {
-
-        private double min = Double.POSITIVE_INFINITY;
-        private double max = Double.NEGATIVE_INFINITY;
-        private double sum;
-        private long count;
+        public String toString() {
+            return min + "/" + round(mean()) + "/" + max;
+        }
     }
 
     public static void main(String[] args) throws IOException {
-        Collector<Measurement, MeasurementAggregator, ResultRow> collector = Collector.of(
-                MeasurementAggregator::new,
-                (a, m) -> {
-                    a.min = Math.min(a.min, m.value);
-                    a.max = Math.max(a.max, m.value);
-                    a.sum += m.value;
-                    a.count++;
-                },
-                (agg1, agg2) -> {
-                    var res = new MeasurementAggregator();
-                    res.min = Math.min(agg1.min, agg2.min);
-                    res.max = Math.max(agg1.max, agg2.max);
-                    res.sum = agg1.sum + agg2.sum;
-                    res.count = agg1.count + agg2.count;
+        long fileSize;
+        TreeMap<String, ResultRow> results = new TreeMap<>();
 
-                    return res;
-                },
-                agg -> {
-                    return new ResultRow(
-                            agg.min,
-                            (Math.round(agg.sum * 10.0) / 10.0) / agg.count,
-                            agg.max);
-                });
+        try (
+            FileChannel channel = FileChannel.open(
+                Paths.get(FILE),
+                StandardOpenOption.READ
+            )
+        ) {
+            fileSize = channel.size();
+            long position = 0;
+            long chunkSize = 1024 * 1024;
 
-        Map<String, ResultRow> measurements = new TreeMap<>(
-                Files.lines(Paths.get(FILE))
-                        .parallel()
-                        .map(l -> new Measurement(l.split(";")))
-                        .collect(groupingByConcurrent(m -> m.station(), collector)));
+            while (position < fileSize) {
+                long size = Math.min(chunkSize, fileSize - position);
+                ByteBuffer buffer = channel.map(
+                    FileChannel.MapMode.READ_ONLY,
+                    position,
+                    size
+                );
 
-        System.out.println(measurements);
+                int bufferSize = buffer.limit();
+                while (buffer.get(bufferSize - 1) != '\n') {
+                    bufferSize--;
+                }
+                buffer.limit(bufferSize);
+                position += bufferSize;
+
+                processBuffer(buffer, results);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(results);
+    }
+
+    private static void processBuffer(
+        ByteBuffer buffer,
+        TreeMap<String, ResultRow> results
+    ) {
+        byte[] lineBuffer = new byte[128];
+        int index = 0;
+
+        while (buffer.hasRemaining()) {
+            byte c = buffer.get();
+            if (c != '\n') {
+                lineBuffer[index] = c;
+                index++;
+            } else {
+                String line = new String(
+                    lineBuffer,
+                    0,
+                    index,
+                    StandardCharsets.UTF_8
+                );
+                processLine(line, results);
+                index = 0;
+            }
+        }
+    }
+
+    private static void processLine(
+        String line,
+        TreeMap<String, ResultRow> results
+    ) {
+        String[] tokens = line.split(";");
+
+        String station = tokens[0];
+        double measurement = Double.parseDouble(tokens[1]);
+
+        results.merge(station, new ResultRow(measurement), (oldVal, newVal) ->
+            oldVal.merge(newVal)
+        );
     }
 }
